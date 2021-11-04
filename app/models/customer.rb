e970a82c35ebe48e68defbed8663ff83e1546d33
edwin sacalamitao <sacalamitao.edwin@yahoo.com.ph>
2021-11-04 10:57:14 +0800

    added: validation to customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
 	# has_and_belongs_to_many :products
 	has_many :transactions, dependent: :destroy
+	validates :name, :address, :contact_number, presence: true
 end
